21d378f154aa6e7cc5ae238204680f456105ad60
coinangler17 <robinmulder1205@outlook.com>
2024-10-15 16:47:30 +0900

    added the Bet-Game for  myproject

--- app/lib/projects.ts
+++ app/lib/projects.ts
@@ -1,5 +1,11 @@
 export const projects = [
     {
+        title: "Bet-Game Figma design",
+        description: "This is my Figma design",
+        image: "/projectsImg/Bet-Game.png",
+        link: "https://www.figma.com/design/9s4vhNZjg9GIQguV2lWezo/Bet-Game"
+    },
+    {
         title: "NFTRaffleGame",
         description: "Here, you need to register with your wallet first. If there's any room created by anyone, then you can join the room and can buy tickets.Or you can create your raffle room with your ...",
         image: "/projectsImg/NFTRaffleGame1.png",

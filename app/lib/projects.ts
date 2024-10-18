0aa7130bbb56c5745d27e58b5f14008eff9bf969
coinangler17 <robinmulder1205@outlook.com>
2024-10-18 09:33:08 +0900

    added the PriaShop project

--- app/lib/projects.ts
+++ app/lib/projects.ts
@@ -6,15 +6,21 @@ export const projects = [
         link: "https://www.figma.com/design/9s4vhNZjg9GIQguV2lWezo/Bet-Game"
     },
     {
-        title: "NFTRaffleGame",
-        description: "Here, you need to register with your wallet first. If there's any room created by anyone, then you can join the room and can buy tickets.Or you can create your raffle room with your ...",
-        image: "/projectsImg/NFTRaffleGame1.png",
-        link: "https://init-raffle-front-54z4.vercel.app/"
+        title: "PriaShop",
+        description: "Introducing a cutting-edge e-commerce platform built with Next.js, designed to support multiple stores effortlessly.",
+        image: "/projectsImg/priashop1.png",
+        link: "https://www.priashop.com/"
     },
     {
         title: "coinNFTMarketplace",
         description: "Here, you need to register with your wallet first. If there's any room created by anyone, then you can join the room and can buy tickets.Or you can create your raffle room with your ...",
         image: "/projectsImg/coinNFTMarketplace.png",
         link: "https://cosmic-nextjs-marketplace.vercel.app/"
-    }
+    },
+    {
+        title: "NFTRaffleGame",
+        description: "Here, you need to register with your wallet first. If there's any room created by anyone, then you can join the room and can buy tickets.Or you can create your raffle room with your ...",
+        image: "/projectsImg/NFTRaffleGame1.png",
+        link: "https://init-raffle-front-54z4.vercel.app/"
+    },
 ]
